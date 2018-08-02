c6f0ad75c0efb1fe01014d3cb38c828c53256a02
Robert Utterback <robertu13@gmail.com>
2018-08-02 14:30:40 -0500

    Adding test script

---
+++ test/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#set -x
+T=20
+
+for N in 2 4 8 32; do
+ 		export CILK_NWORKERS=$N
+		printf "N=$N:\t"
+ 		for i in {1..100}; do
+				for m in record replay; do
+   					PORR_MODE=$m timeout $T ./spinlock &> ${m}.log;
+						ret=$?
+						if [[ "$ret" -ne 0 ]]; then
+									 if [[ "$ret" -eq 124 ]]; then printf "\nTimeout "; else printf "\nCrash "; fi
+									 printf "during $m\n"
+									 exit
+   					fi
+   			printf "."
+				done
+   	done
+  	printf "\nFinished with $N\n"
+done
+
